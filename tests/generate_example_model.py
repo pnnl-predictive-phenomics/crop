03b93a9e8225581c36ee109d2d5ccab072b2b88e
August <30163079+augeorge@users.noreply.github.com>
2024-01-26 11:05:27 -0800

    new dev branch for AG test

--- tests/generate_example_model.py
+++ tests/generate_example_model.py
@@ -1,42 +1,43 @@
-import cobra
 from pprint import pprint
-from cobra.io import write_sbml_model, validate_sbml_model
-from cobra.util.array import create_stoichiometric_matrix
+
+import cobra
 import pandas as pd
+from cobra.io import validate_sbml_model, write_sbml_model
+from cobra.util.array import create_stoichiometric_matrix
 
+pd.options.display.float_format = "{:.0f}".format
 
-pd.options.display.float_format = '{:.0f}'.format
+mets = ["A", "B", "C", "D", "E", "F", "A_ext", "C_ext", "D_ext", "E_ext", "F_ext"]
 
-mets = ['A', 'B', 'C', 'D', 'E', 'F', 'A_ext', 'C_ext', 'D_ext', 'E_ext',
-        'F_ext']
+internal_mets = [met for met in mets if "ext" not in met]
+rxns = ["R_{}".format(i) for i in range(1, 11)]
 
-internal_mets = [met for met in mets if 'ext' not in met]
-rxns = ['R_{}'.format(i) for i in range(1, 11)]
+data = {
+    "R_1": pd.Series({"A": 1, "A_ext": -1}),
+    "R_2": pd.Series({"A": -1, "B": 1}),
+    "R_3": pd.Series({"A": -1, "C": 1}),
+    "R_4": pd.Series({"B": -1, "D": 2, "E": -1}),
+    "R_5": pd.Series({"E": 1, "E_ext": -1}),
+    "R_6": pd.Series({"B": -2, "C": 1, "F": 1}),
+    "R_7": pd.Series({"C": -1, "D": 1}),
+    "R_8": pd.Series({"D": -1, "D_ext": 1}),
+    "R_9": pd.Series({"F": -1, "F_ext": 1}),
+    "R_10": pd.Series({"C": -1, "C_ext": 1}),
+}
 
-data = {'R_1': pd.Series({'A': 1, 'A_ext': -1}),
-        'R_2': pd.Series({'A': -1, 'B': 1}),
-        'R_3': pd.Series({'A': -1, 'C': 1}),
-        'R_4': pd.Series({'B': -1, 'D': 2, 'E': -1}),
-        'R_5': pd.Series({'E': 1, 'E_ext': -1}),
-        'R_6': pd.Series({'B': -2, 'C': 1, 'F': 1}),
-        'R_7': pd.Series({'C': -1, 'D': 1}),
-        'R_8': pd.Series({'D': -1, 'D_ext': 1}),
-        'R_9': pd.Series({'F': -1, 'F_ext': 1}),
-        'R_10': pd.Series({'C': -1, 'C_ext': 1})}
+model = cobra.Model("example_model")
 
-model = cobra.Model('example_model')
-
-A = cobra.Metabolite('A_c', compartment='c')
-A_e = cobra.Metabolite('A_e', compartment='e')
-B = cobra.Metabolite('B_c', compartment='c')
-C = cobra.Metabolite('C_c', compartment='c')
-C_e = cobra.Metabolite('C_e', compartment='e')
-D = cobra.Metabolite('D_c', compartment='c')
-D_e = cobra.Metabolite('D_e', compartment='e')
-E = cobra.Metabolite('E_c', compartment='c')
-E_e = cobra.Metabolite('E_e', compartment='e')
-F = cobra.Metabolite('F_c', compartment='c')
-F_e = cobra.Metabolite('F_e', compartment='e')
+A = cobra.Metabolite("A_c", compartment="c")
+A_e = cobra.Metabolite("A_e", compartment="e")
+B = cobra.Metabolite("B_c", compartment="c")
+C = cobra.Metabolite("C_c", compartment="c")
+C_e = cobra.Metabolite("C_e", compartment="e")
+D = cobra.Metabolite("D_c", compartment="c")
+D_e = cobra.Metabolite("D_e", compartment="e")
+E = cobra.Metabolite("E_c", compartment="c")
+E_e = cobra.Metabolite("E_e", compartment="e")
+F = cobra.Metabolite("F_c", compartment="c")
+F_e = cobra.Metabolite("F_e", compartment="e")
 
 model.add_metabolites([A, B, C, D, E, F, A_e, C_e, D_e, E_e, F_e])
 
@@ -46,84 +47,101 @@ model.add_boundary(model.metabolites.get_by_id("D_e"), type="exchange")
 model.add_boundary(model.metabolites.get_by_id("E_e"), type="exchange")
 model.add_boundary(model.metabolites.get_by_id("F_e"), type="exchange")
 
-ex1 = cobra.Reaction('EX_A')
-ex1.add_metabolites({
-    A_e: -1.0,
-    A: 1.0,
-})
+ex1 = cobra.Reaction("EX_A")
+ex1.add_metabolites(
+    {
+        A_e: -1.0,
+        A: 1.0,
+    }
+)
 
-ex2 = cobra.Reaction('EX_C')
-ex2.add_metabolites({
-    C: -1.0,
-    C_e: 1.0,
+ex2 = cobra.Reaction("EX_C")
+ex2.add_metabolites(
+    {
+        C: -1.0,
+        C_e: 1.0,
+    }
+)
 
-})
+ex3 = cobra.Reaction("EX_D")
+ex3.add_metabolites({D: -1.0, D_e: 1.0})
 
-ex3 = cobra.Reaction('EX_D')
-ex3.add_metabolites({
-    D: -1.0,
-    D_e: 1.0
+ex4 = cobra.Reaction("EX_E")
+ex4.add_metabolites(
+    {
+        E_e: -1.0,
+        E: 1.0,
+    }
+)
 
-})
+ex5 = cobra.Reaction("SINK_F")
+ex5.add_metabolites(
+    {
+        F: -1.0,
+        F_e: 1.0,
+    }
+)
 
-ex4 = cobra.Reaction('EX_E')
-ex4.add_metabolites({
-    E_e: -1.0,
-    E: 1.0,
-})
+r1 = cobra.Reaction("R_A_to_B")
+r1.add_metabolites(
+    {
+        A: -1.0,
+        B: 1.0,
+    }
+)
 
-ex5 = cobra.Reaction('SINK_F')
-ex5.add_metabolites({
-    F: -1.0,
-    F_e: 1.0,
-})
+r2 = cobra.Reaction("R_BE_to_D")
+r2.add_metabolites(
+    {
+        B: -1.0,
+        E: -1.0,
+        D: 2.0,
+    }
+)
 
-r1 = cobra.Reaction('R_A_to_B')
-r1.add_metabolites({
-    A: -1.0,
-    B: 1.0,
-})
+r3 = cobra.Reaction("R_A_to_C")
+r3.add_metabolites(
+    {
+        A: -1.0,
+        C: 1.0,
+    }
+)
 
-r2 = cobra.Reaction('R_BE_to_D')
-r2.add_metabolites({
-    B: -1.0,
-    E: -1.0,
-    D: 2.0,
-})
+r4 = cobra.Reaction("R_BC_to_F")
+r4.add_metabolites({B: -2.0, C: 1.0, F: 1})
 
-r3 = cobra.Reaction('R_A_to_C')
-r3.add_metabolites({
-    A: -1.0,
-    C: 1.0,
-})
-
-r4 = cobra.Reaction('R_BC_to_F')
-r4.add_metabolites({
-    B: -2.0,
-    C: 1.0,
-    F: 1
-})
-
-r5 = cobra.Reaction('R_C_to_D')
-r5.add_metabolites({
-    C: -1.0,
-    D: 1.0,
-})
+r5 = cobra.Reaction("R_C_to_D")
+r5.add_metabolites(
+    {
+        C: -1.0,
+        D: 1.0,
+    }
+)
 
 model.add_reactions([ex1, ex2, ex3, ex4, ex5, r1, r2, r3, r4, r5])
-model.objective = model.reactions.get_by_id('EX_D')
+model.objective = model.reactions.get_by_id("EX_D")
 
-write_sbml_model(model, filename='example_model.xml')
-report = validate_sbml_model(filename='example_model.xml')
+write_sbml_model(model, filename="example_model.xml")
+report = validate_sbml_model(filename="example_model.xml")
 pprint(report)
 
 
-stoich_matrix = create_stoichiometric_matrix(model, 'DataFrame')
+stoich_matrix = create_stoichiometric_matrix(model, "DataFrame")
 
-fullS = pd.DataFrame(data, columns=rxns, index=mets, dtype='int64').fillna(0)
+fullS = pd.DataFrame(data, columns=rxns, index=mets, dtype="int64").fillna(0)
 
-order = ['EX_A', 'R_A_to_B', 'R_A_to_C', 'R_BE_to_D', 'EX_E', 'R_BC_to_F',
-         'R_C_to_D', 'EX_D', 'SINK_F', 'EX_C', ]
+order = [
+    "EX_A",
+    "R_A_to_B",
+    "R_A_to_C",
+    "R_BE_to_D",
+    "EX_E",
+    "R_BC_to_F",
+    "R_C_to_D",
+    "EX_D",
+    "SINK_F",
+    "EX_C",
+]
 
 stoich_matrix = stoich_matrix[order]
 
--- tests/test_crop.py
+++ tests/test_crop.py
@@ -1,61 +1,294 @@
-"""Test functionalities of gap filling."""
-import cobra.io
-import pathlib
+import cobra as cb
+import optlang as op
+import pandas as pd
+import pytest
 
-path = pathlib.Path(__file__).parent
+import crop
+from crop import (
+    PhenotypeObservation,
+    generate_decision_variables,
+    get_steady_state_dual_constraint,
+    model_from_stoich_matrix,
+)
 
+# steps
+# 1. port ABC toy model into COBRA model and test it (metabolites, S, etc)
+# 2. load and validate 'observations' using dataclass
+# 3. generate and validate constraints for CROP problem
+# 4. generate and validate predictions from CROP
 
-def load_model():
-    return cobra.io.read_sbml_model(
-        path.joinpath('example_model.xml').__str__()
-    )
+# note: might be easier to start w/ one condition and FBA constraints first
+#       then move to multiple conditions and full MILP (2 test cases)
 
+### fixtures for testing
 
-def test_no_grow_e():
-    # B + E  nutrient condition is no growth
-    model = cobra.io.read_sbml_model(
-        path.joinpath('example_model.xml').__str__()
-    )
-    media = {'EX_E_e': 100}
-    model.medium = media
-    obj_func = model.slim_optimize()
-    assert obj_func == 0.0
+### copied from notebook - double check!!
 
+# # stoichiometry
+# S_index = ['A_int', 'B_int', 'C_int']
+# S_dict = {'A_SRC->A_int': [1,0,0],
+#           'A_int->B_int': [-1,1,0],
+#           'B_int->C_int': [0,-1,1],
+#           'C_int->C_SNK': [0,0,-1],
+#           'A_int->C_int': [-1,0,1],
+#           'B_SRC->B_int': [0,1,0],
+#           }
 
-def test_no_grow_a():
-    # A alone is growth
-    model = cobra.io.read_sbml_model(
-        path.joinpath('example_model.xml').__str__()
-    )
-    media = {'EX_A_e': 100}
-    model.medium = media
-    obj_func = model.slim_optimize()
-    assert obj_func == 100.0
-
-
-def test_no_grow_4():
-    # A + v3 knockout is no-growth
-    model = cobra.io.read_sbml_model(
-        path.joinpath('example_model.xml').__str__()
-    )
-    media = {'EX_A_e': 100}
-    model.remove_reactions(model.reactions.get_by_id('R_A_to_C'))
-    model.medium = media
-    obj_func = model.slim_optimize()
-
-    assert obj_func == 50.0
+# S_table = pd.DataFrame( S_dict ,index=S_index)
 
 
-def test_no_grow_5():
-    #A + E + v3 knockout is growth
-    model = cobra.io.read_sbml_model(
-        path.joinpath('example_model.xml').__str__()
-    )
-    media = {
-        'EX_A_e': 100,
-        'EX_E_e': 100
+# mets, rxns = S_table.index, S_table.columns
+# n_mets = len(mets)
+# n_rxns = len(rxns)
+
+
+# # constant Omega
+# upper_flux_bound = 1e3
+
+# # flux bounds
+# min_growth_flux = 10
+# max_nogrowth_flux = 5
+
+# # media conditions
+# A_not_B_medium = {
+#  'A_SRC->A_int': min_growth_flux,
+#  'B_SRC->B_int': 0.0
+#  }
+
+# B_not_A_medium = {
+#  'A_SRC->A_int': 0.0,
+#  'B_SRC->B_int': min_growth_flux
+#  }
+
+# # knockout reactions
+# reaction_knockouts = {
+#  'A_int->C_int': True,
+#  }
+
+# ### TODO - IN PROGRESS
+# # list of U_nogrowth and U_growth (reaction size)
+# U_growth = {rxn_id:upper_flux_bound for rxn_id in rxns}
+# U_nogrowth = {rxn_id:upper_flux_bound for rxn_id in rxns}
+# U_growth['A_SRC->A_int'] = min_growth_flux
+# U_growth['B_SRC->B_int'] = 0
+# growth_objective = {rxn_id:1 if rxn_id=='C_int->C_SNK' else 0 for rxn_id in rxns}
+# upper_flux_bounds = {rxn_id:0 for rxn_id in rxns}
+# lower_flux_bounds = {rxn_id:0 for rxn_id in rxns}
+
+# phenotype_observations = {
+#     'A_not_B': PhenotypeObservation(medium=A_not_B_medium, reaction_knockouts={}, gene_knockouts=dict(), growth_phenotype=True),
+#     'B_not_A': PhenotypeObservation(medium=B_not_A_medium, reaction_knockouts={}, gene_knockouts=dict(), growth_phenotype=True),
+#     'A_not_B_ko_AC':PhenotypeObservation(medium=A_not_B_medium, reaction_knockouts=reaction_knockouts, gene_knockouts=dict(), growth_phenotype=False),
+#     'B_not_A_ko_AC':PhenotypeObservation(medium=B_not_A_medium, reaction_knockouts=reaction_knockouts, gene_knockouts=dict(), growth_phenotype=True)
+# }
+
+# cobra_model = model_from_stoich_matrix(S_table, "ABC_toy_model_3", obj=growth_objective, )
+
+# # list of weights (reaction size)
+# likelihoods = [0.5 for _ in rxns]
+
+# # list of z (reaction size)
+# reaction_indicator = {rxn_id:op.Variable(f"z_{rxn_id}", type="binary") for rxn_id in rxns}
+
+# # flux dual, metabolite dual, and flux variables (replaced with function)
+# flux_dual = {}
+# flux = {}
+# metabolite_dual = {}
+# for observation_id, observation in phenotype_observations.items():
+#     if observation.growth_phenotype: # primal problem when growth is observed
+#         flux[observation_id] = {rxn_id:op.Variable(f"v_{observation_id}_{rxn_id}") for rxn_id in rxns}
+#     else: # dual problem when no growth is observed
+#         flux_dual[observation_id] = {rxn_id:op.Variable(f"r_{observation_id}_{rxn_id}") for rxn_id in rxns}
+#         metabolite_dual[observation_id] = {met_id:op.Variable(f"m_{observation_id}_{met_id}") for met_id in mets}
+
+# flux, flux_dual, metabolite_dual = generate_decision_variables(cobra_model, phenotype_observations)
+
+# gibbs_like_constraint = get_steady_state_dual_constraint(cobra_model, phenotype_observations, growth_objective)
+
+# # list of z (reaction size)
+# reaction_indicator = {rxn_id:op.Variable(f"z_{rxn_id}", type="binary") for rxn_id in rxns}
+
+# # list of r (no growth)
+# flux_dual = {rxn_id:op.Variable(f"r_nogrowth_{rxn_id}") for rxn_id in rxns}
+
+# # list of v (growth)
+# flux = {rxn_id:op.Variable(f"v_growth_{rxn_id}") for rxn_id in rxns}
+
+# # list of m (metabolite size)
+# metabolite_dual = {met_id:op.Variable(f"m_nogrowth_{met_id}") for met_id in mets}
+
+# # list of e_c (reaction size)
+
+##### Fixtures #####
+
+
+@pytest.fixture
+def s_table():
+    S_index = ["A_int", "B_int", "C_int"]
+    S_dict = {
+        "A_SRC->A_int": [1, 0, 0],
+        "A_int->B_int": [-1, 1, 0],
+        "B_int->C_int": [0, -1, 1],
+        "C_int->C_SNK": [0, 0, -1],
+        "A_int->C_int": [-1, 0, 1],
+        "B_SRC->B_int": [0, 1, 0],
     }
-    model.remove_reactions(model.reactions.get_by_id('R_A_to_C'))
-    model.medium = media
-    obj_func = model.slim_optimize()
-    assert obj_func > 0.0
+    return pd.DataFrame(S_dict, index=S_index)
+
+
+##### Tests #####
+### COBRA model tests
+def test_load_cobra_model_from_stoichiometry_table(s_table: pd.DataFrame):
+    # load reactions, metabolites, stoichiometry from S_table dataframe
+    cobra_model = model_from_stoich_matrix(s_table)
+    test_cobra_model_metabolites(cobra_model)
+    test_cobra_model_reactions(cobra_model)
+    test_cobra_model_stoichiometric_matrix(cobra_model)
+
+
+def test_cobra_model_metabolites(model: cb.core.model.Model):
+    # expect 'A_int' 'B_int' 'C_int'
+    actual = ...  # get list cobra model metabolite ids for ABC toy model 3
+    expected = ...  # list of metabolite ids for ABC toy model 3
+    raise NotImplementedError
+
+
+def test_cobra_model_reactions(model: cb.core.model.Model):
+    # expect 'A_SRC->int' 'A_int->B_int' 'B_int->C_int' 'C_int->C_SNK'
+    #        'A_int->C_int' 'B_SRC->B_int'
+    actual = ...  # get list cobra model reactions for ABC toy model 3
+    expected = ...  # list of reactions for ABC toy model 3
+    raise NotImplementedError
+
+
+def test_cobra_model_stoichiometric_matrix(model: cb.core.model.Model):
+    # expect ...
+    actual = ...  # get cobra model S matrix for ABC toy model 3
+    expected = ...  # S matrix for ABC toy model 3
+    raise NotImplementedError
+
+
+###
+
+# test
+
+
+# test growth objective set correctly (e.g. C_int->C_SNK)
+def test_growth_objective():
+    actual = ...  # get growth objective for ABC toy model 3
+    expected = ...  # growth objective for ABC toy model 3
+    raise NotImplementedError
+
+
+### MILP constraints
+# test steady state dual constraint variables (Gibbs-like S^T*m + e_c = r_nogrowth)
+def test_get_steady_state_dual_constraint(model, phenotype_observations, growth_objective):
+    actual_constraint = get_steady_state_dual_constraint(
+        model, phenotype_observations, growth_objective
+    )
+    expected_constraint = {}  # print out constraints and check by hand
+    assert actual_constraint == expected_constraint
+
+
+# dual flux variable constraint (r_nogrowth_i < Omega*(1-z_i) ...)
+def test_get_dual_flux_variable_constraint():
+    actual = ...  # get optlang dual flux constraint for ABC toy model 3
+    expected = ...  # dual flux constraints for ABC toy model 3
+    raise NotImplementedError
+
+
+# steady state flux constraint (Sv_growth=0)
+def test_steady_state_flux_constraint():
+    actual = ...  # get optlang steady state flux constraint for ABC toy model 3
+    expected = ...  # steady state flux constraints for ABC toy model 3
+    raise NotImplementedError
+
+
+# flux variable constraint (0<=v_growth_i<=U_growth_i*z_i ...)
+def test_flux_constraint():
+    actual = ...  # get optlang flux constraints for ABC toy model 3
+    expected = ...  # flux constraints for ABC toy model 3
+    raise NotImplementedError
+
+
+# reaction constraints (z = 1 for import reactions)
+def test_reaction_constraints():
+    actual = ...  # get optlang reaction selector constraints for ABC toy model 3
+    expected = ...  # reaction selector constraints for ABC toy model 3
+    raise NotImplementedError
+
+
+# mixed integer test
+def test_mixed_integer_constraint():
+    actual = ...  # get Z type
+    expected = ...  # Z should be mixed integer (binary)
+    raise NotImplementedError
+
+
+### CROP predictions for ABC toy model 3
+def test_CROP_prediction_ABCmodel3():
+    """
+    Test case 1:
+    Current model has A-->B reaction
+    True model doesn't have A-->B reaction
+
+    Conditions
+    1. observe growth with A in media but not B
+    2. observe growth with B in media but not A
+    3. observe no growth with A in media but not B - with a knockout for A-->C
+    4. observe growth with B in the media but not A - with a knockout for A-->C
+
+    We expect CROP to remove A-->B reaction (z=0 for A-->B reaction)
+    """
+    raise NotImplementedError
+
+
+### CROP predictions for ABC toy model 3
+def test_CROP_prediction_ABCmodel3_consistency():
+    """
+    Test case 1:
+    Current model has A-->B reaction
+    True model doesn't have A-->B reaction
+
+    Conditions
+    1. observe growth with A in media but not B
+    2. observe growth with B in media but not A
+    3. observe no growth with A in media but not B - with a knockout for A-->C
+    4. observe growth with B in the media but not A - with a knockout for A-->C
+
+    We expect CROP to remove A-->B reaction (z=0 for A-->B reaction)
+    Furthermore, this solution should satisfy the constraints given
+    """
+    raise NotImplementedError
+
+
+### Other tests
+
+
+# test decision variable generation
+def test_generate_decision_variables(
+    model: cb.core.model.Model, phenotype_observations: PhenotypeObservation
+):
+    # check that flux, flux dual, and metabolite dual variables are as generated expected
+    raise NotImplementedError
+
+
+# test flux bounds function(s)
+def test_generate_flux_bounds_from_growth_observation(phenotype_observation: PhenotypeObservation):
+    raise NotImplementedError
+
+
+def test_generate_flux_dual_bounds_from_nogrowth_observation(
+    phenotype_observation: PhenotypeObservation,
+):
+    raise NotImplementedError
+
+
+# test PhenotypeObservation data class
+def test_phenotype_observation():
+    raise NotImplementedError
+
+
+# test ConsistentReproductionOfPhenotype class
+def test_ConsistentReproductionOfPhenotype_class():
+    raise NotImplementedError
